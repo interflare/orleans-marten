ba94f925c6215097ed56c4596306e2d523a0b4de
Matt Crook <mattlcrook@icloud.com>
2025-10-21 19:13:15 +1300

    test(integration): make use of testcontainers over manual docker integration

--- test/Orleans.Providers.Marten.Integration.Tests/Infrastructure/Fixtures/PostgresDatabaseFixture.cs
+++ test/Orleans.Providers.Marten.Integration.Tests/Infrastructure/Fixtures/PostgresDatabaseFixture.cs
@@ -1,7 +1,5 @@
-using Docker.DotNet;
-using Docker.DotNet.Models;
-using System.Data.Common;
 using System.Diagnostics.CodeAnalysis;
+using Testcontainers.PostgreSql;
 
 namespace Orleans.Providers.Marten.Integration.Tests.Infrastructure.Fixtures;
 
@@ -14,71 +12,28 @@ namespace Orleans.Providers.Marten.Integration.Tests.Infrastructure.Fixtures;
 [ExcludeFromCodeCoverage]
 public class PostgresDatabaseFixture : IAsyncLifetime
 {
-    private const string ServerImage = "postgres:16";
-    private static readonly DockerClient Client = new DockerClientConfiguration().CreateClient();
-
-    private readonly string _containerId;
+    private readonly PostgreSqlContainer _postgresContainer;
     public readonly string ConnectionString;
 
-    private PostgresDatabaseFixture(string containerId, string connectionString)
+    private PostgresDatabaseFixture(PostgreSqlContainer postgresContainer)
     {
-        _containerId = containerId;
-        ConnectionString = connectionString;
+        _postgresContainer = postgresContainer;
+        ConnectionString = postgresContainer.GetConnectionString();
     }
 
     internal static async Task<PostgresDatabaseFixture> CreateAsync(string name, CancellationToken token = default)
     {
-        await Client.Images.CreateImageAsync(new ImagesCreateParameters { FromImage = ServerImage }, null, new Progress<JSONMessage>(), token);
+        var postgresContainer = new PostgreSqlBuilder()
+            .WithImage("postgres:16")
+            .WithName(name)
+            .WithPortBinding(5432, true)
+            .Build();
 
-        var password = Guid.NewGuid().ToString();
-        var createResponse = await Client.Containers.CreateContainerAsync(
-            new CreateContainerParameters
-            {
-                Image = ServerImage,
-                Name = name,
-                HostConfig = new HostConfig
-                {
-                    PortBindings = new Dictionary<string, IList<PortBinding>>
-                    {
-                        ["5432/tcp"] = [new PortBinding { HostPort = "0" }] // Random host port
-                    }
-                },
-                Env = ["POSTGRES_DB=postgres", "POSTGRES_USER=postgres", $"POSTGRES_PASSWORD={password}"]
-            }, token);
+        await postgresContainer.StartAsync(token);
 
-        var containerId = createResponse.ID;
-        try
-        {
-            if (!await Client.Containers.StartContainerAsync(containerId, null, token))
-                throw new InvalidOperationException("could not start container");
-
-            var inspect = await Client.Containers.InspectContainerAsync(containerId, token);
-
-            var dbPort = inspect.NetworkSettings.Ports["5432/tcp"].FirstOrDefault()?.HostPort;
-            if (dbPort is null)
-                throw new InvalidOperationException("could not determine host port");
-
-            var connectionParams = new DbConnectionStringBuilder
-            {
-                ["Server"] = "127.0.0.1",
-                ["Port"] = dbPort,
-                ["Database"] = "postgres",
-                ["User Id"] = "postgres",
-                ["Password"] = password
-            };
-
-            return new PostgresDatabaseFixture(containerId, connectionParams.ToString());
-        }
-        catch
-        {
-            await DeleteAsync(containerId, token);
-            throw;
-        }
+        return new PostgresDatabaseFixture(postgresContainer);
     }
 
-    private static async Task DeleteAsync(string containerId, CancellationToken token = default)
-        => await Client.Containers.RemoveContainerAsync(containerId, new ContainerRemoveParameters { Force = true }, token);
-
     public Task InitializeAsync() => Task.CompletedTask;
-    public Task DisposeAsync() => DeleteAsync(_containerId);
+    public Task DisposeAsync() => _postgresContainer.DisposeAsync().AsTask();
 }
